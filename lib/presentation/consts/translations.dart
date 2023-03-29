ce85cf7039c2b4318dd65829e3f10752cee4ef76
Neikan <sladkov.e.m@outlook.com>
2023-03-29 15:46:23 +0300

    feat(consts): add translations

---
+++ lib/presentation/consts/translations.dart
@@ -0,0 +1,11 @@
+// Project imports:
+import 'package:app_hive/presentation/consts/keys.dart';
+
+const Map<String, String> labelsCategories = {
+  keyTitle: 'Categories',
+};
+
+const Map<String, String> labelsNotFound = {
+  keyTitle: 'Error',
+  keyContent: 'Page not found',
+};
